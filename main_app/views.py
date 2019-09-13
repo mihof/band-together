00818bf51335ed8c63cce277fd1bf9986e51141c
Loquen Jones <lrjones416@gmail.com>
2019-09-13 16:16:56 -0700

    remove commented out code

--- main_app/views.py
+++ main_app/views.py
@@ -32,30 +32,3 @@ def signup(request):
   return render(request, 'registration/signup.html', context)
 
 # Create your views here.
-# class CustomerSignUpView(CreateView):
-#     model = CustomUser
-#     form_class = CustomerSignUpForm
-#     template_name = 'registration/signup_form.html'
-
-#     def get_context_data(self, **kwargs):
-#         kwargs['user_type'] = 'customer'
-#         return super().get_context_data(**kwargs)
-
-#     def form_valid(self, form):
-#         user = form.save()
-#         login(self.request, user)
-#         return redirect('')
-
-# class ManagerSignUpView(CreateView):
-#     model = CustomUser
-#     form_class = ManagerSignUpForm
-#     template_name = 'registration/signup_form.html'
-
-#     def get_context_data(self, **kwargs):
-#         kwargs['user_type'] = 'manager'
-#         return super().get_context_data(**kwargs)
-
-#     def form_valid(self, form):
-#         user = form.save()
-#         login(self.request, user)
-#         return redirect('')
