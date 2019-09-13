90117402c86dc859b57ccac3b9bfb36febafdf65
Michael <michaelhofmann987@gmail.com>
2019-09-12 21:27:34 -0700

    Added and set up urls file

--- main_app/models.py
+++ main_app/models.py
@@ -26,3 +26,6 @@ class Event(models.Model):
   artists = models.CharField(max_length=100)
   description = models.TextField(max_length=800)
   date = models.DateField('date of show')
+
+  def __str__(self):
+    return self.artists
