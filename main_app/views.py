5955f95f1f6e58f5a0295e51e6e9e9e1db2f816e
Michael <michaelhofmann987@gmail.com>
2019-09-12 22:03:58 -0700

    Added templates directory, base and home files. Added first route, and first views render for home

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Venue, Event
+
+def home(request):
+  return render(request, 'home.html')
 
 # Create your views here.
