d85b27bd152e869ade749e126cc9bf94ed70fb47
Michael <michaelhofmann987@gmail.com>
2019-09-15 12:17:29 -0700

    added get_absolute_url to event model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 from datetime import date
 # Create your models here.
@@ -35,6 +36,9 @@ class Event(models.Model):
 
   def __str__(self):
     return self.artists
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'event_id': self.id})
 
   venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
 
