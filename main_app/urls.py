334e8c16456582c591247b7dba033f6c80965018
Michael <michaelhofmann987@gmail.com>
2019-09-15 14:17:30 -0700

    added edit and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('events/', views.event_index, name='index'),
   path('events/<int:event_id>/', views.event_detail, name='detail'),
   path('events/create/', views.EventCreate.as_view(), name='create'),
+  path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='update'),
+  path('events/<int:pk>/delete/', views.EventDelete.as_view(), name='delete'),
 ]
