0754c79d72319b0c4958ec95ff6170eb2444bb1b
Loquen Jones <lrjones416@gmail.com>
2019-09-16 10:40:59 -0700

    Update user profile to save when a new user is created with a default role of Customer

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from  .models import Venue, Event
+from  .models import Venue, Event, Profile
 
 admin.site.register(Venue)
 admin.site.register(Event)
+admin.site.register(Profile)
 
 # Register your models here.
