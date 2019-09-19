f3158a838b0329632e5e9b0f679dad6c88e8b8ea
Michael <michaelhofmann987@gmail.com>
2019-09-19 10:33:04 -0700

    Cleaned up models- unnecessary quantity options

--- main_app/models.py
+++ main_app/models.py
@@ -16,18 +16,6 @@ ROLES = (
   ('M', 'Manager')
 )
 
-QUANTITY = (
-  ('0', 0),
-  ('1', 1),
-  ('2', 2),
-  ('3', 3),
-  ('4', 4),
-  ('5', 5),
-  ('6', 6),
-  ('7', 7),
-  ('8', 8),
-)
-
 class Venue(models.Model):
   name = models.CharField(max_length=100)
   address = models.CharField(max_length=200)
